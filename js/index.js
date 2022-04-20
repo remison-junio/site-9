fd0479d7cdea4a9b88ad6ee43ccb6ca114b10e6e
Remison Junio <remisonpires@gmail.com>
2022-04-20 05:30:20 -0300

    Ajustes finais, e animações do scroll

--- js/index.js
+++ js/index.js
@@ -3,4 +3,59 @@ $(document).ready(()=> {
 		e.preventDefault()
 		$('#barra-navegacao, .btn-menu').toggleClass('active')
 	})
+
+	$(window).scroll((e)=> {
+		if($(e.target).scrollTop() >= 100) {
+			$('header').addClass('fixo')
+		} else if($(e.target).scrollTop() == 0) {
+			$('header').removeClass('fixo')
+		}
+	})
+
+	$('.menu a[href^="#"]').on('click', (e)=> {
+		e.preventDefault()
+
+		$('#barra-navegacao, .btn-menu').removeClass('active')
+		
+		let id = $(e.target).attr('href'),
+			targetOffset = $(id).offset().top;
+
+		$('html, body').animate({ 
+			scrollTop: targetOffset - 50
+		}, 800);
+	})
+
+	const debounce = function(func, wait, immediate) {
+	  let timeout;
+	  return function(...args) {
+	    const context = this;
+	    const later = function () {
+	      timeout = null;
+	      if (!immediate) func.apply(context, args);
+	    };
+	    const callNow = immediate && !timeout;
+	    clearTimeout(timeout);
+	    timeout = setTimeout(later, wait);
+	    if (callNow) func.apply(context, args);
+	  };
+	};
+	
+
+	function animacao(){
+		$.each($('[data-animacao]'), (indice, valor)=> {
+			const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+
+			if(windowTop > valor.offsetTop){
+				$(valor).addClass('animar')
+			}
+		})
+	}
+	
+	animacao()
+	
+	if($('[data-animacao]').length) {
+	  $(window).scroll(debounce(function() {
+	   animacao()
+	  }, 200))
+	}
 })
